a268a0aefbf223317c82118e05924dddc92940e2
Innocentem <mwbzinno@gmail.com>
2023-09-22 03:10:08 -0700

    main.h

--- main.h
+++ main.h
@@ -5,18 +5,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 int _printf(const char *format, ...);
 int ptchr(char c);
 int ptss(char *c);

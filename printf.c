9b6cbb67e61bd5edd1ce1692851ac5618f378b8c
DESIREAK001 <desirengonzikabagyo@gmail.com>
2023-09-21 23:07:20 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,42 +1,44 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _printf: prints formated string to stdopt
- * @format: format specifier
+ * main - Entry point
  *
- * Returns: Nmbr of bytes
+ * Return: Always 0
  */
-
-int _printf(const char *format, ...)
+int main(void)
 {
-	int i, str_ct, count = 0;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-	va_list args;
-	va_start(args, format);
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		ptchr(format[i]);
-	
-	}
-	if (format[i] == '%' && format[i + 1] == 'c')
-	{
-		ptchr(va_arg(args, int));
-			count += 1;
-	
-	}
-	if (format[i + 1] == 's')
-	{
-		str_ct = va_arg(args, int);
-			i++;
-		count = (str_ct - 1);
-	}
-	else if (format[i + 1] == '%')
-	{
-		ptchr('%');
-	
-	}
-	count += 1;
-
-	return (count);
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
